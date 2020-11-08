e9dc062a159b04e83bf6bed0ff77dbc045c1d797
Rodolphe <rodolphem1997@gmail.com>
2020-11-09 00:20:18 +0100

    modif code

--- README.md
+++ README.md
@@ -2,6 +2,13 @@ app
 
 ################# LANCEMENT DE L'APPLICATION #######################
 
+si les lignes :
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+ne sont pas présentes, alors faire les deux manipulation ci dessous :
+
 pour démarer le serveur : export FLASK_APP=__init__.py
 
 pour activer le debugger : export FLASK_DEBUG=1
--- __init__.py
+++ __init__.py
@@ -1,6 +1,16 @@
 from flask import Flask 		#importing Flask class from flask package to use flask function
 app = Flask(__name__)			#instanciating imported flask class
 
+
+@app.route('/home')	
 @app.route('/')					#flask class object ("app") decorator to change de function and generate HTML
 def hello():					#decorated function
 	return "Hello World"
+
+@app.route('/about')			
+def about():					
+	return "<h1>About Page</h1>"
+
+
+if __name__ == '__main__':		#Start the flask server when we do a "python __init__.py" ib the terminal (we don't need to export some variables)
+	app.run(debug=True)
