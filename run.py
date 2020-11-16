5c0e8457cc35e660ae5f5c7f08f4920fe50e9b51
Rodolphe <rodolphem1997@gmail.com>
2020-11-16 11:12:09 +0100

    gitignore base de données

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from webapp import app 									#import from __init__.py file in webapp package the app variable who is an instance of Flask class
+from webapp import create_db
 
 if __name__ == '__main__':								#Start the flask server when we do a "python run.py" ib the terminal (we don't need to export some variables)
 	app.run(debug=True)
